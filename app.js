e57b674308ca3391f456ac0a36eff79d6434ceb7
Gatsjy <gkswndks123@naver.com>
2021-04-05 22:36:04 +0900

    2021-04-05, 한주안, express 실습

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-const express = require('express')
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res)=>{
+    res.send('hello express');
+});
+
+app.get('/fastcampus', (req,res)=>{
+    res.send('fastcampus express');
+});
+
+app.listen(port, () => {
+    console.log('Express listening on port', port);
+});
