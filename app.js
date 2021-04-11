882529255cf558e018e183923739cccb1f60046e
Gatsjy <gkswndks123@naver.com>
2021-04-11 17:45:48 +0900

    2021-04-11, 한주안, express 스터디2

--- app.js
+++ app.js
@@ -3,48 +3,89 @@ const nunjucks = require('nunjucks');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 
-const admin = require('./routes/admin');
-const contacts = require('./routes/contacts');
-const app = express();
-const port = 3000;
 
-nunjucks.configure('template',{
-    autoescape : true ,
-    express : app
-});
+class App {
 
-// 미들웨어 셋팅
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended : false}));
-app.use('/uploads', express.static('uploads'));
+    constructor () {
+        this.app = express();
+        
+        // 뷰엔진 셋팅
+        this.setViewEngine();
 
-// 전역변수 세팅하는 방법
-app.use((req,res,next) => {
-    app.locals.isLogin = true;
-    next();
-});
+        // 미들웨어 셋팅
+        this.setMiddleWare();
 
-app.get('/', (req, res)=>{
-    res.send('hello express');
-});
+        // 정적 디렉토리 추가
+        this.setStatic();
 
-function vipMiddleware(req,res,next){
-    console.log('최우선 미들웨어');
-    next();
+        // 로컬 변수
+        this.setLocals();
+
+        // 라우팅
+        this.getRouting();
+
+        // 404 페이지를 찾을수가 없음
+        this.status404();
+
+        // 에러처리
+        this.errorHandler();
+
+
+    }
+
+
+    setMiddleWare (){
+        
+        // 미들웨어 셋팅
+        this.app.use(logger('dev'));
+        this.app.use(bodyParser.json());
+        this.app.use(bodyParser.urlencoded({ extended: false }));
+
+    }
+
+    setViewEngine (){
+
+        nunjucks.configure('template', {
+            autoescape: true,
+            express: this.app
+        });
+
+    }
+
+
+    setStatic (){
+        this.app.use('/uploads', express.static('uploads'));
+    }
+
+    setLocals(){
+
+        // 템플릿 변수
+        this.app.use( (req, res, next) => {
+            this.app.locals.isLogin = true;
+            this.app.locals.req_path = req.path;
+            next();
+        });
+
+    }
+
+    getRouting (){
+        this.app.use(require('./controllers'))
+    }
+
+    status404() {        
+        this.app.use( ( req , res, _ ) => {
+            res.status(404).render('common/404.html')
+        });
+    }
+
+    errorHandler() {
+
+        this.app.use( (err, req, res,  _ ) => {
+            res.status(500).render('common/500.html')
+        });
+    
+    }
+
 }
 
-app.use('/admin', vipMiddleware, admin);
-
-app.use((req,res, _)=>{
-    res.status(400).render('common/404.html')
-})
-
-app.use((req,res, _)=>{
-    res.status(500).render('common/500.html')
-})
-app.use('/contacts', contacts);
-
-app.listen(port, () => {
-    console.log('Express listening on port', port);
-});
+module.exports = new App().app;
