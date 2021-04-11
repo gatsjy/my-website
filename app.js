c439d10ba77bff6c17542c3148c49f5f4ac759bd
Gatsjy <gkswndks123@naver.com>
2021-04-11 17:09:29 +0900

    2021-04-11, 한주안, Express 공부

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const nunjucks = require('nunjucks');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
 
 const admin = require('./routes/admin');
 const contacts = require('./routes/contacts');
@@ -11,11 +13,36 @@ nunjucks.configure('template',{
     express : app
 });
 
+// 미들웨어 셋팅
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : false}));
+app.use('/uploads', express.static('uploads'));
+
+// 전역변수 세팅하는 방법
+app.use((req,res,next) => {
+    app.locals.isLogin = true;
+    next();
+});
+
 app.get('/', (req, res)=>{
     res.send('hello express');
 });
 
-app.use('/admin', admin);
+function vipMiddleware(req,res,next){
+    console.log('최우선 미들웨어');
+    next();
+}
+
+app.use('/admin', vipMiddleware, admin);
+
+app.use((req,res, _)=>{
+    res.status(400).render('common/404.html')
+})
+
+app.use((req,res, _)=>{
+    res.status(500).render('common/500.html')
+})
 app.use('/contacts', contacts);
 
 app.listen(port, () => {
--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,34 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/',(req,res) =>{
+function testMiddleWare(req,res,next){
+    console.log('첫번째 미들웨어');
+    next();
+}
+
+function testMiddleWare2(req,res,next){
+    console.log('두번째 미들웨어');
+    next()
+}
+router.get('/',testMiddleWare, testMiddleWare2,(req,res) =>{
+    // 미들웨어 중간 요청을 가로챈다.
     res.send('admin 이후 url');
 });
 
 router.get('/products',(req,res) =>{
     //res.send('admin products 이후 url');
     res.render('admin/products.html',{
-        message : 'hello!!!!!!!!!2222',
+        message : `<h1>태그가 출력됩니다.</h1>`,
         online : 'express'
     });
+});
+
+router.get('/products/write', (req,res)=>{
+    res.render('admin/write.html');
+});
+
+router.post('/products/write', (req,res)=>{
+    res.send(req.body)
 });
 
 module.exports = router;
